95d26b632ce22dc0d1a249c3150b184e5abe3620
Devraj-jha <devrajjha100806@gmail.com>
2025-09-16 09:53:54 +0530

    initial

---
+++ quizz/src/App.jsx
@@ -0,0 +1,11 @@
+// src/App.jsx
+function App() {
+  return (
+    <div>
+      <h1>🚀 React Quiz App</h1>
+      <p>Welcome to your first React project!</p>
+    </div>
+  );
+}
+
+export default App;
